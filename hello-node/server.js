ed7d0d51e3793f312564142cdd9dfe86810f3ab6
mickymount <m.tiburu@gmail.com>
2017-04-18 22:23:59 -0400

    modified server.js

--- hello-node/server.js
+++ hello-node/server.js
@@ -1,8 +1,7 @@
 var http = require('http');
-var datetime = new Date();
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello World, from COE!" + datetime);
+  response.end("Hello World, from COE!" + new Date());
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
